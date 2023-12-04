6fc38f49ae064374bcacac1d5917b1e9b766b697
Nivaldo <badivia.f@gmail.com>
2023-12-03 22:14:13 -0300

    fuck this, ill day 3 part two try again tomorrow

--- 2023/03/solution.ts
+++ 2023/03/solution.ts
@@ -50,12 +50,13 @@ function partOne() {
 				next = nextLine.slice(leftDiagonal, rightDiagonal);
 			}
 
-			if (
+			const symbolWithinReach =
 				(!!previousChar && previousChar.match(SYMBOLS_REGEX)) ||
 				(!!nextChar && nextChar.match(SYMBOLS_REGEX)) ||
 				prev.match(SYMBOLS_REGEX) ||
-				next.match(SYMBOLS_REGEX)
-			) {
+				next.match(SYMBOLS_REGEX);
+
+			if (symbolWithinReach && symbolWithinReach.length > 0) {
 				sum += Number(number);
 			}
 
@@ -69,56 +70,63 @@ function partOne() {
 }
 
 function partTwo() {
-	const SYMBOLS_REGEX = /[^.\d]/g;
+	const DIGITS_REGEX = /\d+/g;
 
 	console.time("partTwo");
 
 	let sum = 0;
 	const lines = (TEST_INPUT || input).split("\n");
 
-	for (let [lineIndex, line] of lines.entries()) {
-		const numbersMatch = line.match(/\d+/g);
+	for (const [lineIndex, line] of lines.entries()) {
+		const gearMatch = line.match(/\*/g);
 
-		if (!numbersMatch) continue;
+		if (!gearMatch) continue;
+
 		const previousLine = lines[lineIndex - 1];
 		const nextLine = lines[lineIndex + 1];
 
-		for (const number of numbersMatch) {
-			const startIndex = line.indexOf(number);
+		for (const gear of gearMatch) {
+			const startIndex = line.indexOf(gear);
 
 			let prev = "";
 			let next = "";
+
 			const previousChar = line.charAt(startIndex - 1);
-			const nextChar = line.charAt(startIndex + number.length);
+			const nextChar = line.charAt(startIndex + gear.length);
 
 			if (previousLine) {
 				const leftDiagonal = previousLine.charAt(startIndex - 1) ? startIndex - 1 : startIndex;
-				const rightDiagonal = previousLine.charAt(startIndex + number.length + 1)
-					? startIndex + number.length + 1
-					: startIndex + number.length;
+				const rightDiagonal = previousLine.charAt(startIndex + gear.length + 1)
+					? startIndex + gear.length + 1
+					: startIndex + gear.length;
 
 				prev = previousLine.slice(leftDiagonal, rightDiagonal);
 			}
 
 			if (nextLine) {
 				const leftDiagonal = nextLine.charAt(startIndex - 1) ? startIndex - 1 : startIndex;
-				const rightDiagonal = nextLine.charAt(startIndex + number.length + 1)
-					? startIndex + number.length + 1
-					: startIndex + number.length;
+				const rightDiagonal = nextLine.charAt(startIndex + gear.length + 1)
+					? startIndex + gear.length + 1
+					: startIndex + gear.length;
 
 				next = nextLine.slice(leftDiagonal, rightDiagonal);
 			}
 
-			if (
-				(!!previousChar && previousChar.match(SYMBOLS_REGEX)) ||
-				(!!nextChar && nextChar.match(SYMBOLS_REGEX)) ||
-				prev.match(SYMBOLS_REGEX) ||
-				next.match(SYMBOLS_REGEX)
-			) {
-				sum += Number(number);
-			}
+			const symbolWithinReach =
+				previousChar.match(DIGITS_REGEX) ||
+				nextChar.match(DIGITS_REGEX) ||
+				prev.match(DIGITS_REGEX) ||
+				next.match(DIGITS_REGEX);
 
-			line = line.replace(number, number.replaceAll(/\d/g, "."));
+			if (symbolWithinReach && symbolWithinReach.length > 0) {
+				console.log(
+					previousChar.match(DIGITS_REGEX),
+					nextChar.match(DIGITS_REGEX),
+					prev.match(DIGITS_REGEX),
+					next.match(DIGITS_REGEX),
+					`\n${prev || ""}\n${previousChar || ""}${gear}${nextChar}\n${next || ""}`,
+				);
+			}
 		}
 	}
 
